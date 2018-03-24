d3f16dc76544a03e784de0c82506b72b5c41fe9b
Nathan Shelly <nathanshelly@u.northwestern.edu>
2018-03-23 19:38:43 -0500

    Convert missed usage of ZSH -> DOTFILES

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,2 @@
 # add topic folders to fpath to enable functions and completion scripts
-for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+for topic_folder ($DOTFILES/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
