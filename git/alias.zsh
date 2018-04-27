69bf8ba7b7aef4b27a396f12cfa509325a52baa5
Nathan Shelly <nathanshelly@u.northwestern.edu>
2018-04-26 22:32:39 -0500

    Adjust organization

--- git/alias.zsh
+++ git/alias.zsh
@@ -4,16 +4,21 @@ command -v hub >/dev/null 2>&1 && {
   alias git=hub
 }
 
-# set some git aliases
+# some git aliases
+# sorted alphabetically
 # use gitconfig aliases
 # to avoid duplication
+alias g='git'
+
 alias ga='git a'
 alias gb='git b'
 alias gba='git b -a'
 alias gbt='git bt'
+
 alias gc='git c'
 alias gca='git c --amend'
 alias gco='git co'
+
 # sed removes -/+ from
 # diffs, use only color
 alias gd='git d | _slice_plus_minus'
@@ -22,12 +27,16 @@ alias gdc='git d --cached | _slice_plus_minus'
 # vice versa, use to see deleted whitespace
 alias gdr='git d -R | _slice_plus_minus'
 alias gdcr='git d --cached -R | _slice_plus_minus'
+
 alias gl='git l'
 alias glg='git lg'
+
 alias gr='git r -i'
 alias grc='git r --continue'
+
 alias gs='git s -sb'
 alias gst='git st'
 alias gstl='git st list'
 alias gstsp='git st show -p'
+
 alias gu='git undo'
