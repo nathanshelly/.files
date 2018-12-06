f9e86c3b7c2079427bfcf9a975d23bf90b92b2ff
Nathan Shelly <nsnshelly@gmail.com>
2018-12-06 10:34:40 -0600

    Add short alias for `open`

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -6,3 +6,8 @@ alias zshn='zsh -f'
 alias l='ls -CF'
 alias la='ls -AF'
 alias ll='ls -AFhl'
+
+alias brewupdate='brew update && brew upgrade && brew cleanup && brew doctor'
+
+# shortened utilities
+alias op=open
