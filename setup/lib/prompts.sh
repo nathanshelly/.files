7fb46c67ccdd7f3c2230a615f783601c2b1638ee
Nathan Shelly <nsnshelly@gmail.com>
2018-12-27 23:21:14 -0600

    Clean up `prompts`

--- setup/lib/prompts.sh
+++ setup/lib/prompts.sh
@@ -1,21 +1,37 @@
 #! /usr/bin/env bash
 
-# define colored prompts for user interaction
+# define colored prompts for helpful messages
 
-info () {
-  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+
+# << escape sequences >>
+# refs
+# - https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
+# - https://en.wikipedia.org/wiki/ANSI_escape_code
+# - http://ascii-table.com/ansi-escape-sequences.php
+
+header() {
+  printf "\n\r\033[0;1m$1\033[0m\n\n"
 }
 
-user () {
-  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+info() {
+  printf "  [ \033[00;36m..\033[0m ] $1\n"
 }
 
-success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+# user interaction
+user() {
+  printf "  [ \033[0;35m??\033[0m ] $1\n"
 }
 
-fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+success() {
+  printf "  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+warn() {
+  printf "  [\033[0;33mWARN\033[0m] $1\n"
+}
+
+fail() {
+  printf "  [\033[0;31mFAIL\033[0m] $1\n"
   echo ''
   exit
 }
