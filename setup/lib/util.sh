c8ad7c92fa0c8adc6cd629fbda18d65e3c654bea
Nathan Shelly <nsnshelly@gmail.com>
2019-01-01 01:26:23 -0600

    Document `join_by`

--- setup/lib/util.sh
+++ setup/lib/util.sh
@@ -1,12 +1,14 @@
 #! /usr/bin/env bash
 
-# first argument is separator to join with
-# next n args joined w/ separator
+# join array with given argument
 #
 # refs:
 # - https://stackoverflow.com/questions/22124417/bash-using-ifs-to-join-an-array-into-a-string
 # - https://stackoverflow.com/questions/1527049/how-can-i-join-elements-of-an-array-in-bash
 # - https://stackoverflow.com/questions/5163144/what-are-the-special-dollar-sign-shell-variables
+#
+# Args:
+#   - {string} delimiter - delimiter to join array by
 join_by() {
   local IFS="$1";
   shift;
