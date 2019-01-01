9e27b531a1a443531cc56f2c524901fbd18b774b
Nathan Shelly <nsnshelly@gmail.com>
2019-01-01 01:37:47 -0600

    Compute DOTFILES path

--- setup/lib/check_dotfiles_variable.sh
+++ setup/lib/check_dotfiles_variable.sh
@@ -1,5 +1,16 @@
 #! /usr/bin/env bash
 
+# get path to dotfiles repo root (should be same as $DOTFILES)
+#
+# Returns:
+#   {string} `_COMPUTED_DOTFILES` - path to this repo root
+_get_dotfiles_repo_root() {
+  local dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+  cd "$dir"
+  _COMPUTED_DOTFILES="$(git rev-parse --show-toplevel)"
+  cd - >/dev/null 2>&1
+}
+
 # checks if DOTFILES environment variable is set, sets it or quits based on user
 # input if not
 #
@@ -7,25 +18,22 @@
 #   {string} `DOTFILES` - exports DOTFILES environment variable if user chooses
 #     to set it
 check_dotfiles_variable() {
+  _get_dotfiles_repo_root
+
   [[ -z "$DOTFILES" ]] && {
-    printf "Installation requires a set DOTFILES environment variable.\nThis \
-variable must hold the path to this repo (e.g. on my\nmachine I clone this \
-repo into my home folder so my DOTFILES\nvariable equals \
-'/Users/nathan/.files')\n\nWould you like to set this variable now? \
-(y/any other key) "
-    read -r -n 1 -s maybe_continue
+    printf "This script requires a DOTFILES environment variable holding the \
+path\nto the repo this script is running form. This path appears to be\n\
+'$_COMPUTED_DOTFILES'.\n\nIs this correct (y/any other key)? "
+    read -r -n 1 maybe_continue
 
     [[ "$maybe_continue" == 'y' ]] && {
-      printf "\n\nenter path: DOTFILES="
-      read -r dotfiles_path
-      export DOTFILES="$dotfiles_path"
-      printf "\nDOTFILES set to '$DOTFILES'. Note that this export exists only \
-in this script\nYou'll need to export again (or just close and reopen your \
-terminal) to have DOTFILES\nset in your calling shell (and by extension for \
-the changes made here to take effect\n"
+      export DOTFILES="$_COMPUTED_DOTFILES"
+      printf "\n\nDOTFILES set to '$DOTFILES'. Note that this export exists\
+\nonly within this script.\n"
     } || {
       printf "\n\nTo continue, set DOTFILES \
-(\`export DOTFILES=<path-to-this-repo>\`),\nthen rerun this script\nGoodbye.\n"
+(\`export DOTFILES=<path-to-dotfiles-repo>\`),\nthen rerun this script.\n\
+Goodbye.\n"
       exit
     }
   }
