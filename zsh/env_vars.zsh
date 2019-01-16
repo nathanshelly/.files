42f7a15ae8de07e3de21dc6f725d4ab26aa89c7c
Nathan Shelly <nsnshelly@gmail.com>
2019-01-15 16:39:22 -0800

    Update deprecated Homebrew env variable

--- zsh/env_vars.zsh
+++ zsh/env_vars.zsh
@@ -1,4 +1,5 @@
 # << homebrew >>
+# ref - https://github.com/Homebrew/brew/blob/master/docs/Manpage.md#environment
 
-export HOMEBREW_UPGRADE_CLEANUP=0 # checking existence, not value
+export HOMEBREW_INSTALL_CLEANUP=0 # checking existence, not value
 export HOMEBREW_AUTO_UPDATE_SECS=86400 # check for update every 24 hours
