1afb8fafc0a0ff61a03da9e77eeec8c9b57e261d
Nathan Shelly <nathan.shelly@opendoor.com>
2019-03-30 02:18:45 -0700

    Customize `zsh-autosuggestions` keybindings

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,10 +1,19 @@
 # customizing plugins specified in `$DOTFILES/zsh/plugins.txt`, installed from
 # `$DOTFILES/zsh/plugins.sh` using `antibody`
 
-# << autosuggestions >>
+# <<<< autosuggestions >>>>
 # ref - https://github.com/zsh-users/zsh-autosuggestions#configuration
+
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=244' # light grey
 ZSH_AUTOSUGGEST_USE_ASYNC=true # can be set to anything
+
 # in the future consider testing `completion` strategy on feature branch
 # ref - https://github.com/zsh-users/zsh-autosuggestions/compare/features/completion-suggestions
 ZSH_AUTOSUGGEST_STRATEGY=(history)
+
+# << keymappings >>
+# ref - https://github.com/zsh-users/zsh-autosuggestions#key-bindings
+
+bindkey -M viins '^a' autosuggest-accept # until end of suggestion
+bindkey -M viins '^ ' autosuggest-execute
+bindkey -M viins '^w' forward-word # next word, how `vim` `w` would advance
