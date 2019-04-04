c0cec15b1d2af5c616937b11ead4764d434f2a15
Nathan Shelly <nsnshelly@gmail.com>
2019-04-03 22:13:53 -0700

    Alias `ptpython` -> `ptpython --vim`

--- languages/python/alias.zsh
+++ languages/python/alias.zsh
@@ -1,2 +1,4 @@
 # shorter python alias
-command -v python3 > /dev/null 2>&1 && alias python="$(which python3)"
+command -v python3 > /dev/null 2>&1 && alias python=python3
+
+command -v ptpython > /dev/null 2>&1 && alias ptpython='ptpython --vi'
