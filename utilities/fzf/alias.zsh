1c449d3c6d7cee37c34814922199005437f8c775
Nathan Shelly <nathan.shelly@opendoor.com>
2019-05-01 23:25:08 -0700

    Add more `zsh` `fzf` functions

--- utilities/fzf/alias.zsh
+++ utilities/fzf/alias.zsh
@@ -1,3 +1,7 @@
 # corresponding functions defined in '$DOTFILES/functions/_fzf_functions'
 alias rgz='fzf_rg_edit'
 alias killp='fzf_kill_by_port'
+alias findpath='fzf_find_path'
+alias bif='fzf_brew_install'
+alias bunf='fzf_brew_uninstall'
+alias buf='fzf_brew_upgrade'
