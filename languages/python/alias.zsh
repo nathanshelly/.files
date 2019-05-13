e051619c8736b90d0963536a715c1efe089c8e37
Nathan Shelly <nathan.shelly@opendoor.com>
2019-05-13 11:15:45 -0700

    Alias `pip` -> `pip3`

--- languages/python/alias.zsh
+++ languages/python/alias.zsh
@@ -1,4 +1,5 @@
 # shorter python alias
 command -v python3 > /dev/null 2>&1 && alias python=python3
+command -v pip3 > /dev/null 2>&1 && alias pip=pip3
 
 command -v ptpython > /dev/null 2>&1 && alias ptpython='ptpython --vi'
