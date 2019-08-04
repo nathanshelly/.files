009d4758cd26c55e5813c3555ab38ca01db54e10
Nathan Shelly <nathan.shelly@opendoor.com>
2019-08-04 15:51:57 -0700

    Make Pipenv venvs in project root

--- languages/python/options.zsh
+++ languages/python/options.zsh
@@ -1,2 +1,8 @@
 # prevent generation of .pyc files
 export PYTHONDONTWRITEBYTECODE=1
+
+# make Pipenv venvs in project root, not `$(pipenv --venv) || $WORKON_HOME`
+# refs:
+# - https://docs.pipenv.org/en/latest/advanced/#custom-virtual-environment-location
+# - https://github.com/pypa/pipenv/issues/1049
+export PIPENV_VENV_IN_PROJECT=1
