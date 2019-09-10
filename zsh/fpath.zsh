5fbabd856b1032b1575c6cf7d748c9a7fdf8fccc
Nathan Shelly <nathan.shelly@opendoor.com>
2019-09-10 12:51:52 -0700

    Format `fpath`

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,6 @@
 # add topic folders to fpath to enable functions and completion scripts
-for topic_folder ($DOTFILES/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+for topic_folder ($DOTFILES/*) do
+  if [ -d $topic_folder ]; then
+    fpath=($topic_folder $fpath);
+  fi;
+done
