7f5dc5bb630b6a62164f7557b085ffc6c21cadd7
Nathan Shelly <nathan.shelly@opendoor.com>
2019-09-10 15:32:00 -0700

    Remove unused Ruby path manipulation
    Removing this check saves ~0.1 seconds on each initial shell load.

--- languages/ruby/path.zsh
+++
@@ -1,9 +0,0 @@
-# add local gems to path if ruby is installed
-command -v ruby > /dev/null 2>&1 && {
-  path=(
-    # `-r` loads specified library (`rubygems`)
-    # `-e` specify script to run
-    "$(ruby -r rubygems -e 'puts Gem.user_dir')/bin"
-    $path
-  )
-}
