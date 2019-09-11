595367d5eec5505c6a09a032544b55a4e04a8f9d
Nathan Shelly <nathan.shelly@opendoor.com>
2019-09-10 22:37:35 -0700

    Restructure function sourcing

--- zsh/fpath.zsh
+++
@@ -1,6 +0,0 @@
-# add topic folders to fpath to enable functions and completion scripts
-for topic_folder ($DOTFILES/*) do
-  if [ -d $topic_folder ]; then
-    fpath=($topic_folder $fpath);
-  fi;
-done
