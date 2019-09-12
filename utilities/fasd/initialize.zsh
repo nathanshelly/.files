02e2e5b08352f18dd31ed800a4344b4af5ef6e38
Nathan Shelly <nathan.shelly@opendoor.com>
2019-09-12 13:39:43 -0700

    Speed up `fasd` initialization

--- utilities/fasd/initialize.zsh
+++ utilities/fasd/initialize.zsh
@@ -1,2 +1,15 @@
+command -v fuck || return
+
 # turn on fasd if installed
-command -v fasd > /dev/null 2>&1 && eval "$(fasd --init auto)"
+# use a cache to reduce hit on shell performance
+# ref - https://github.com/clvv/fasd#install
+fasd_cache="$HOME/.fasd_cache"
+# TODO: maybe remove redundant `$(command -v fasd)` here?
+if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+  # load only needed components
+  # - `posix-alias` - defined aliases, e.g. `j`, `f`, etc.
+  # - `zsh-hook` - define _fasd_preexec and add it to zsh preexec array
+  fasd --init posix-alias zsh-hook >| "$fasd_cache"
+fi
+source "$fasd_cache"
+unset fasd_cache
