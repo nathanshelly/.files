0b589263a4e951192e0f20e99ee491315cd9546e
Nathan Shelly <nathan.shelly@opendoor.com>
2019-09-19 17:50:12 -0700

    Remove unused aliases

--- utilities/fzf/alias.zsh
+++ utilities/fzf/alias.zsh
@@ -1,7 +1,3 @@
 # corresponding functions defined in `./functions.zsh`
 alias rgz='fzf_rg_edit'
 alias killp='fzf_kill_by_port'
-alias findpath='fzf_find_path'
-alias bif='fzf_brew_install'
-alias bunf='fzf_brew_uninstall'
-alias buf='fzf_brew_upgrade'
