138c18bfb29f2e4cce9c8df9db8c5a880eab15d5
Nathan Shelly <nathan.shelly@opendoor.com>
2019-09-20 02:03:33 -0700

    Clean up spectrum

--- zsh/spectrum.zsh
+++ zsh/spectrum.zsh
@@ -1,46 +1,24 @@
-#! usr/bin/env zsh
-# A script to make using 256 colors in zsh less painful.
-# P.C. Shyamshankar <sykora@lucentbeing.com>
+#! /usr/bin/env zsh
 #
-# refs:
-#   - https://github.com/sykora/etc
-#   - https://github.com/sykora/etc/blob/master/zsh/functions/spectrum/
+# makes using color codes less painful. Apply a color code with "$FG[...]<text>"
+# (or "$BG[...]<text>") where "..." is the corresponding 3 digit color code
+# (e.g. 001, 089, 127)
+#
+# TODO: figure out where $reset_color is set/how that works
+# ref - https://github.com/sykora/etc/blob/master/zsh/functions/spectrum
 
 typeset -AHg FX FG BG
 
 FX=(
-    reset     "%{[00m%}"
-    bold      "%{[01m%}" no-bold      "%{[22m%}"
-    italic    "%{[03m%}" no-italic    "%{[23m%}"
-    underline "%{[04m%}" no-underline "%{[24m%}"
-    blink     "%{[05m%}" no-blink     "%{[25m%}"
-    reverse   "%{[07m%}" no-reverse   "%{[27m%}"
+  reset     "%{[00m%}"
+  bold      "%{[01m%}" no-bold      "%{[22m%}"
+  italic    "%{[03m%}" no-italic    "%{[23m%}"
+  underline "%{[04m%}" no-underline "%{[24m%}"
+  blink     "%{[05m%}" no-blink     "%{[25m%}"
+  reverse   "%{[07m%}" no-reverse   "%{[27m%}"
 )
 
 for color in {000..255}; do
-    FG[$color]="%{[38;5;${color}m%}"
-    BG[$color]="%{[48;5;${color}m%}"
+  FG[$color]="%{[38;5;${color}m%}"
+  BG[$color]="%{[48;5;${color}m%}"
 done
-
-
-ZSH_SPECTRUM_TEXT=${ZSH_SPECTRUM_TEXT:-Arma virumque cano Troiae qui primus ab oris}
-
-# Show all 256 colors with color number
-function spectrum_vls() {
-  for code in {000..255}; do
-    print -P -- "$code: %{$FG[$code]%}$ZSH_SPECTRUM_TEXT%{$reset_color%}"
-  done
-}
-
-function spectrum_ls() {
-  for i in {0..255}; do
-    print -Pn "%${i}F${(l:3::0:)i}%f " ${${(M)$((i%8)):#7}:+$'\n'};
-  done
-}
-
-# Show all 256 colors where the background is set to specific color
-function spectrum_bls() {
-  for code in {000..255}; do
-    print -P -- "$code: %{$BG[$code]%}$ZSH_SPECTRUM_TEXT%{$reset_color%}"
-  done
-}
