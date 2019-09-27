171240604709e78bfd8636f93be058da5549185f
Nathan Shelly <nathan.shelly@opendoor.com>
2019-09-27 14:13:23 -0400

    Alias `emp` -> open and launch MarkdownPreview

--- vim/alias.zsh
+++ vim/alias.zsh
@@ -1,3 +1,7 @@
 # `enc` for `editor no config`, `-u <config file>`
- # would need to update with new editors
+# would need to update with new editors
 alias enc="$EDITOR -u /dev/null"
+
+# `emp` for `editor markdown preview`, `-c MarkdownPreview`
+# requires https://github.com/iamcco/markdown-preview.nvim
+alias emp="$EDITOR -c MarkdownPreview"
