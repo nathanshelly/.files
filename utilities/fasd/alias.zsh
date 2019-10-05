14864cb5aa594b4051eb2d29bf5fb043462bfe4f
Nathan Shelly <nathan.shelly@opendoor.com>
2019-10-04 18:36:47 -0700

    Avoid loading `fasd` aliases
    Don't clutter shell with `fasd` defined aliases, define only those that
    I actually use.

--- utilities/fasd/alias.zsh
+++ utilities/fasd/alias.zsh
@@ -1,5 +1,5 @@
 # access files/directories based on "frecency" (frequency & recency)
 # ref - https://github.com/clvv/fasd
 
-alias ez='f -e $EDITOR' # open file with $EDITOR
+alias ez='fasd -f -e $EDITOR' # open file with $EDITOR
 alias j='fasd_cd -d' # j alias instead of default z
