38f729f607c19fcbac939a202802e206ab0fde9f
Nathan Shelly <nathan.shelly@opendoor.com>
2019-10-04 18:37:23 -0700

    Fix ripgrep aliases

--- utilities/ripgrep/alias.zsh
+++ utilities/ripgrep/alias.zsh
@@ -2,32 +2,29 @@
 # ref - https://github.com/BurntSushi/ripgrep
 
 # TODO: remove once habit broken
-alias rg='echo "use generic g, not specific rg (use /usr/local/bin/rg if needed)"'
+alias rg='echo "use generic f, not specific rg (use /usr/local/bin/rg if needed)"'
 # # use smart case by default
-# alias rg='rg --smart-case'
+# alias f='f --smart-case'
 
 # increase context around match
-alias rgc='rg --context 3'
+alias fcc='f --context 3'
 
 # search for literal strings
-alias rgf='rg --fixed-strings'
+alias ff='f --fixed-strings'
 
 # show only matching filenames
-alias rgl='rg --files-with-matches'
-
-# ignore tests - exclude any filename/directory matching the given patterns
-alias rgnt="rg --glob '!*test*' --glob '!*spec*'"
+alias fl='f --files-with-matches'
 
 # search only tests - any filename/directory matching the given patterns
-alias rgt="rg --glob '*test*' --glob '*spec*'"
+alias ft="f --glob '*test*' --glob '*spec*'"
 
-# force case sensitivity
-alias rgs='rg --case-sensitive'
+# ignore tests - exclude any filename/directory matching the given patterns
+alias fnt="f --glob '!*test*' --glob '!*spec*'"
 
 # `-uuu` (`--unrestricted`) ignores .gitignore files, and searches hidden files
 # and directories along with binary files
-alias rgu='rg -uuu'
-alias rgus='rg -uuu --case-sensitive'
+alias fu='f -uuu'
+alias fus='f -uuu --case-sensitive'
 
 # whole word
-alias rgw='rg --word-regexp'
+alias fw='f --word-regexp'
