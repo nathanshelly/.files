70df228bba2be9e379159bfe04bf1c8f0cdb7af3
Nathan Shelly <nathan.shelly@opendoor.com>
2019-10-24 11:06:53 -0700

    Remove pipenv config

--- languages/python/env_vars.zsh
+++ languages/python/env_vars.zsh
@@ -1,8 +1,2 @@
 # prevent generation of .pyc files
 export PYTHONDONTWRITEBYTECODE=1
-
-# make Pipenv venvs in project root, not `$(pipenv --venv) || $WORKON_HOME`
-# refs:
-# - https://docs.pipenv.org/en/latest/advanced/#custom-virtual-environment-location
-# - https://github.com/pypa/pipenv/issues/1049
-export PIPENV_VENV_IN_PROJECT=1
