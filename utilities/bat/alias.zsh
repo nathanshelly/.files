53181e400dfa61d394390ec1dbf7d63b4cfcd3e0
Nathan Shelly <nathan.shelly@opendoor.com>
2019-10-28 13:09:50 -0700

    Alias `cat` to `bat`/`mdcat`

--- utilities/bat/alias.zsh
+++ utilities/bat/alias.zsh
@@ -1,3 +1,6 @@
 # `cat` w/ color
 # ref - https://github.com/sharkdp/bat
-command -v bat > /dev/null && alias cat='bat'
+if command -v bat > /dev/null && command -v mdcat > /dev/null; then
+  # $DOTFILES/functions/smat
+  alias cat=smat
+fi
