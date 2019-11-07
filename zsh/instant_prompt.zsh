ee35af7cd90c16afd7854dcef3ee161d94ab0934
Nathan Shelly <nathan.shelly@opendoor.com>
2019-11-07 00:56:51 -0800

    Configure new `powerlevel10k` options

---
+++ zsh/instant_prompt.zsh
@@ -0,0 +1,9 @@
+# load prompt piece by piece for faster rendering
+# ref - https://github.com/romkatv/powerlevel10k#what-is-instant-prompt
+
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block, everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
