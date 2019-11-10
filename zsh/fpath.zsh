46e953dad3949839c84050fe601a15fab0c41fc5
Nathan Shelly <nathan.shelly@opendoor.com>
2019-11-09 20:28:56 -0800

    Add `fpath.zsh`

---
+++ zsh/fpath.zsh
@@ -0,0 +1,7 @@
+# enable completions for homebrew-installed programs
+# ref - https://docs.brew.sh/Shell-Completion#configuring-completions-in-zsh
+if command -v brew &> /dev/null; then
+  # hardcoding path given by following command for performance
+  # `$(brew --prefix)/share/zsh/site-functions`
+  fpath=("/usr/local/share/zsh/site-functions" $fpath)
+fi
