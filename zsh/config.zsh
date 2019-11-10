98c4d3954c7d59f16951f279ebcae8ae62c9c0c0
Nathan Shelly <nathan.shelly@opendoor.com>
2019-11-09 20:29:13 -0800

    Add documentation

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,3 +1,6 @@
-HISTFILE="$HOME/.zsh_history"
+# number of lines of history kept by the shell
 HISTSIZE=100000
+# file where history is saved
+HISTFILE="$HOME/.zsh_history"
+# number of lines saved to $HISTFILE
 SAVEHIST=$HISTSIZE
