72769fcf38ae7a64060b58a2159478e87b21ff63
Nathan Shelly <nathan.shelly@opendoor.com>
2019-11-24 14:21:37 -0800

    Add 🦀 identifier to Rust

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -405,9 +405,11 @@
 
   #################[ rust_version: rustc version (https://www.rust-lang.org) ]##################
   # Rust version color.
-  typeset -g POWERLEVEL9K_RUST_VERSION_FOREGROUND=37 # teal
+  typeset -g POWERLEVEL9K_RUST_VERSION_FOREGROUND=9 # light red
   # Show rust version only when in a rust project subdirectory.
   typeset -g POWERLEVEL9K_RUST_VERSION_PROJECT_ONLY=true
+  # custom icon
+  typeset -g POWERLEVEL9K_RUST_VERSION_VISUAL_IDENTIFIER_EXPANSION='🦀'
 
   # Transient prompt works similarly to the builtin transient_rprompt option. It trims down prompt
   # when accepting a command line. Supported values:
