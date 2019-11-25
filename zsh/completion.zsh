231607c6d47c115f803218d4a05363c0e2fd34a0
Nathan Shelly <nathan.shelly@opendoor.com>
2019-11-24 20:08:58 -0800

    Move `brew` completion initialization

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -18,3 +18,8 @@ unset _comp_files
 # case insensitive completion
 # ref - https://superuser.com/questions/1092033/how-can-i-make-zsh-tab-completion-fix-capitalization-errors-for-directorys-and
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+
+# enable completions for homebrew-installed programs
+# TODO: fix whatever's not working here
+# ref - https://docs.brew.sh/Shell-Completion#configuring-completions-in-zsh
+command -v brew &> /dev/null && fpath=("$HOMEBREW_PREFIX/share/zsh/site-functions" $fpath)
--- zsh/fpath.zsh
+++
@@ -1,5 +0,0 @@
-# enable completions for homebrew-installed programs
-# ref - https://docs.brew.sh/Shell-Completion#configuring-completions-in-zsh
-if command -v brew &> /dev/null; then
-  fpath=("$HOMEBREW_PREFIX/share/zsh/site-functions" $fpath)
-fi
