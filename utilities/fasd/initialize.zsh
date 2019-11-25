a19b04041d09247947fc41c7f631ff655690027e
Nathan Shelly <nathan.shelly@opendoor.com>
2019-11-24 20:42:03 -0800

    Clean up conditionals

--- utilities/fasd/initialize.zsh
+++ utilities/fasd/initialize.zsh
@@ -5,10 +5,10 @@ command -v fasd > /dev/null 2>&1 || return
 # ref - https://github.com/clvv/fasd#install
 fasd_cache="$HOME/.fasd_cache"
 # TODO: maybe remove redundant `$(command -v fasd)` here?
-if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+[ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ] && {
   # load only needed component
   # - `zsh-hook` - define _fasd_preexec and add it to zsh preexec array
   fasd --init zsh-hook >| "$fasd_cache"
-fi
+}
 source "$fasd_cache"
 unset fasd_cache
