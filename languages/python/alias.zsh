165ac2619147b5da8901d005fb37be0ba67b412c
Nathan Shelly <nsnshelly@gmail.com>
2019-12-07 16:58:08 -0800

    Remove unneeded python/pip aliases
    Subsumed by the switch to `asdf`.

--- languages/python/alias.zsh
+++ languages/python/alias.zsh
@@ -1,7 +1,3 @@
 #! /usr/bin/env zsh
 
-# shorter python alias
-command -v python3 > /dev/null 2>&1 && alias python=python3
-command -v pip3 > /dev/null 2>&1 && alias pip=pip3
-
 command -v ptpython > /dev/null 2>&1 && alias ptpython='ptpython --vi'
