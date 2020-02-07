daa11ed97d44ff7e283216a74fc942061a67a503
Nathan Shelly <nsnshelly@gmail.com>
2020-02-07 11:47:16 -0800

    Clean up `smat` aliasing

--- utilities/bat/alias.zsh
+++ utilities/bat/alias.zsh
@@ -1,10 +1,3 @@
 #! /usr/bin/env zsh
 
-command -v bat > /dev/null || return
-
-if command -v smat > /dev/null && command -v mdcat > /dev/null; then
-  # $DOTFILES/functions/smat
-  alias cat=smat
-else
-  alias cat=bat
-fi
+command -v smat > /dev/null && alias cat=smat # $DOTFILES/functions/smat
