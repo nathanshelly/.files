39844673ffff4f7e38f6e192d58e632ba57a054d
Nathan Shelly <nsnshelly@gmail.com>
2020-02-09 18:47:09 -0800

    Handle missing `jq` & add to Brewfile

--- git/pager.zsh
+++ git/pager.zsh
@@ -1,7 +1,7 @@
 #! /usr/bin/env zsh
 
 TERMINAL_THEME="$($DOTFILES/infra/scripts/get_local_setting.sh "theme")"
-if [ $TERMINAL_THEME = light ]; then
+if [ ${TERMINAL_THEME:-""} = light ]; then
   PAGER_THEME="OneHalfLight";
 else
   # default to dark theme
--- infra/scripts/get_local_setting.sh
+++ infra/scripts/get_local_setting.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+command -v jq > /dev/null || exit 0
+
 # `-raw-output` strips quotes from strings
 # ref - https://github.com/stedolan/jq/issues/1735#issuecomment-427863218
 jq --raw-output --arg KEY "$1" '.[$KEY]' < "$DOTFILES/.files-settings.json"
