a0acf98861fe236fec0925c6e5bd43f7a3b774d7
Nathan Shelly <nsnshelly@gmail.com>
2020-04-03 11:23:37 -0700

    Move `LESSOPEN` to `dynamic_env_vars`
    In order to ensure that `highlight` is accessible

--- zsh/dynamic_env_vars.zsh
+++ zsh/dynamic_env_vars.zsh
@@ -2,3 +2,8 @@
 
 # get editor from local settings
 export EDITOR="$($DOTFILES/infra/scripts/get_local_setting.sh "editor")"
+
+# LESSOPEN configures preprocessor that colors ANSI escape sequences
+# http://www.andre-simon.de/doku/highlight/en/integration.php
+export LESSOPEN="| $(command -v highlight) %s --out-format truecolor \
+--line-numbers --force --style base16/onedark --no-trailing-nl"
