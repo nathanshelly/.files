49b8d8c42b9601eccd5479d345f3c647139df8ba
Nathan Shelly <nsnshelly@gmail.com>
2020-05-13 23:33:20 -0500

    Remove no longer relevant TODOs

--- neovim/alias.zsh
+++ neovim/alias.zsh
@@ -2,7 +2,6 @@
 
 # `enc` for `editor no config`, `-u <config file>`
 # would need to update with new editors
-# TODO: check that this still works (if so setting `backspace` behavior in `options.vim` may no longer be necessary)
 alias enc="$EDITOR -u /dev/null"
 
 # `emp` for `editor markdown preview`, `-c MarkdownPreview`
