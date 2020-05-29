ddf1c30f9ce06f10f22cbc515199f4da585fd87c
Nathan Shelly <nsnshelly@gmail.com>
2020-05-28 21:03:40 -0500

    refactor: set git pager theme via `GIT_PAGER`
    Instead of running `git config ...` on each shell startup, set pager via
    environment variable.

--- zsh/dynamic_env_vars.zsh
+++ zsh/dynamic_env_vars.zsh
@@ -7,3 +7,24 @@ export EDITOR="$($DOTFILES/infra/scripts/get_local_setting.sh "editor")"
 # http://www.andre-simon.de/doku/highlight/en/integration.php
 export LESSOPEN="| $(command -v highlight) %s --out-format truecolor \
 --line-numbers --force --style base16/onedark --no-trailing-nl"
+
+set_git_pager_theme() {
+  local color_overrides pager_theme terminal_theme
+
+  # dynamically set pager based on color theme in `$DOTFILES/.files-settings.json`
+  terminal_theme="$($DOTFILES/infra/scripts/get_local_setting.sh "theme")"
+  if [ ${terminal_theme:-""} = light ]; then
+    pager_theme="OneHalfLight";
+  else
+    # default to dark theme
+    pager_theme="TwoDark"
+    color_overrides=" --plus-color='#1c3428' --plus-emph-color='#37664e'\
+      --minus-color='#390423' --minus-emph-color='#af0d6c'"
+  fi
+
+  # https://git-scm.com/docs/git-config#Documentation/git-config.txt-corepager
+  export GIT_PAGER="delta --tabs=2 --hunk-style=plain --highlight-removed\
+    --theme='$pager_theme'${color_overrides}"
+}
+
+set_git_pager_theme
