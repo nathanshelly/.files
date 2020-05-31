73aaddc0c14d6b820000182a0d32cb04392bf4f9
Nathan Shelly <nsnshelly@gmail.com>
2020-05-31 02:09:02 -0500

    refactor: clean up `bat` configuration

--- zsh/dynamic_env_vars.zsh
+++ zsh/dynamic_env_vars.zsh
@@ -1,12 +1,33 @@
-# environment variables that require dependencies to set (currently just `jq`)
+# environment variables that cannot be set in zsenv because:
+#
+# - requiring a dependency to set (currently just `jq`)
+# - they would be overwritten by system files sourced between `zshenv` & `zshrc`
 
-# get editor from local settings
-export EDITOR="$($DOTFILES/infra/scripts/get_local_setting.sh "editor")"
+main() {
+  set_bat_theme
+  set_git_pager_theme
 
-# LESSOPEN configures preprocessor that colors ANSI escape sequences
-# http://www.andre-simon.de/doku/highlight/en/integration.php
-export LESSOPEN="| $(command -v highlight) %s --out-format truecolor \
+  # get editor from local settings
+  export EDITOR="$($DOTFILES/infra/scripts/get_local_setting.sh "editor")"
+
+  # LESSOPEN configures preprocessor that colors ANSI escape sequences
+  # http://www.andre-simon.de/doku/highlight/en/integration.php
+  export LESSOPEN="| $(command -v highlight) %s --out-format truecolor \
 --line-numbers --force --style base16/onedark --no-trailing-nl"
+}
+
+# set `bat` theme based on terminal theme in `$DOTFILES/.files-settings.json`
+set_bat_theme() {
+  local terminal_theme
+
+  terminal_theme="$($DOTFILES/infra/scripts/get_local_setting.sh "theme")"
+  if [ ${terminal_theme:-""} = light ]; then
+    export BAT_THEME="GitHub";
+  else
+    # default to dark theme
+    export BAT_THEME="TwoDark";
+  fi
+}
 
 set_git_pager_theme() {
   local color_overrides pager_theme terminal_theme
@@ -27,4 +48,4 @@ set_git_pager_theme() {
     --theme='$pager_theme'${color_overrides}"
 }
 
-set_git_pager_theme
+main "$@"
