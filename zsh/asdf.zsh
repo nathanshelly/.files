f518565e6748250ebd558a4e8b4c160c9e2e79b2
Nathan Shelly <nsnshelly@gmail.com>
2020-07-06 16:17:44 -0500

    Manage `asdf` via `git`, not `brew`

--- zsh/asdf.zsh
+++ zsh/asdf.zsh
@@ -4,19 +4,29 @@
 # initialization is done here so that it can be sourced in `./zshrc`
 # after all other `zsh` config files have been sourced to avoid conflicts.
 #
+# ref - https://asdf-vm.com/#/core-manage-asdf-vm?id=add-to-your-shell
+#
 # see more details on `asdf` and my global config in `$DOTFILES/utilities/asdf`
 
 "$DOTFILES/infra/scripts/component_enabled.sh" 'asdf' || return 0
 
-HOMEBREW_PREFIX="$(brew --prefix)"
-# ref - https://asdf-vm.com/#/core-manage-asdf-vm?id=add-to-your-shell
+
 if [ -f "$HOME/.asdf/asdf.sh" ]; then
   # git installation (method used by `$DOTFILES/infra/setup/bin/setup_asdf.sh`)
   source "$HOME/.asdf/asdf.sh"
-elif [ -f "$HOMEBREW_PREFIX/opt/asdf/asdf.sh" ]; then
-  source "$HOMEBREW_PREFIX/opt/asdf/asdf.sh"
-else
-  echo 'Error initializing `asdf`. Try running `$DOTFILES/infra/setup/bin/setup_asdf.sh`.'
-  exit 0
+  return 0
 fi
-unset HOMEBREW_PREFIX
+
+source_homebrew_asdf() {
+  local path_to_homebrew_asdf="$(brew --prefix)/opt/asdf/asdf.sh"
+
+  [ -f "$path_to_homebrew_asdf" ] || return 1
+
+  source "$path_to_homebrew_asdf"
+  return 0
+}
+source_homebrew_asdf && return 0
+
+echo 'Error initializing `asdf`. Try running `$DOTFILES/infra/setup/bin/setup_asdf.sh`.'
+echo 'Or setting `asdf: false` in $DOTFILES/.files-settings.json'
+return 0
