d248a8baa20c7b3f825dae7cbc745e6cdd4b52c2
Nathan Shelly <nsnshelly@gmail.com>
2020-07-26 00:42:32 -0500

    Remove packages from home-manager config

--- nix/home.nix
+++ nix/home.nix
@@ -4,46 +4,6 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  home.packages = with pkgs; [
-    asciinema
-    bandwhich
-    bat
-    darwin.trash # TODO: understand behavior on non-Darwin system
-    direnv
-    dust
-    exa
-    exiftool
-    fd
-    ffmpeg-full
-    fzf
-    git
-    git-lfs
-    gitAndTools.delta
-    gitAndTools.gh
-    gitAndTools.hub
-    gitAndTools.tig
-    gnumake
-    gnupg
-    hyperfine
-    jq
-    lua # z.lua dependency
-    mdcat
-    ncurses
-    neovim
-    nodejs
-    python3
-    ripgrep
-    rnix-lsp # nix lsp
-    rsync
-    shellcheck
-    shfmt
-    tldr
-    tmux
-    tree
-    vim
-    watchexec
-    zsh
-  ];
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
