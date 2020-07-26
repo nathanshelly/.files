e9c1149ff5113aee5656a101352ab2a560f8cf49
Nathan Shelly <nsnshelly@gmail.com>
2020-07-26 00:41:49 -0500

    Put user profile first on path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -13,6 +13,11 @@ typeset -U path # make path array unique (a set)
 # python dependency manager
 [[ -d "$HOME/.poetry/bin" ]] && path=("$HOME/.poetry/bin" $path)
 
+# put nix profile first on path
+[[ -d "/etc/profiles/per-user/$USER/bin" ]] && {
+  path=("/etc/profiles/per-user/$USER/bin" $path)
+}
+
 # where `pip install --user` installs executables
 [[ -d "$HOME/.local/bin" ]] && path=("$HOME/.local/bin" $path)
 
