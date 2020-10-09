b52a80dff0039de6fd7cd2c96e5aa9ed8b94d71c
Nathan Shelly <nsnshelly@gmail.com>
2020-10-08 21:17:49 -0400

    move xvimrc -> `gui.nix`

--- nix/gui.nix
+++ nix/gui.nix
@@ -7,8 +7,6 @@ let
 in
 {
   home.file = {
-    ".xvimrc".source = "${vars.DOTFILES}/gui/xvimrc";
-
     "${vars.XDG_CONFIG_HOME}/alacritty/alacritty.yml".source =
       "${GUI_APPS}/alacritty/alacritty.yml";
 
@@ -30,5 +28,41 @@ in
 
     "${APPLICATION_SUPPORT}/Code/User/settings.json".source =
       "${GUI_APPS}/vscode/settings.json";
+
+    ".xvimrc".text = ''
+      " Options
+
+      set wrap
+      set number
+
+      set incsearch
+      set hlsearch
+
+      set ignorecase
+      set smartcase
+
+      " Mappings
+
+      inoremap jk <esc>
+
+      nnoremap J }
+      vnoremap J }
+      nnoremap K {
+      vnoremap K {
+
+      nnoremap z $
+      vnoremap z $h
+      nnoremap Z ^
+      vnoremap Z ^
+
+      nmap == :xcmenucmd Re-Indent<CR>
+      vmap = :xcmenucmd Re-Indent<CR>
+
+      nmap <C-i> :xcmenucmd Go Forward<CR>
+      nmap <C-o> :xcmenucmd Go Back<CR>
+
+      nmap <C-z> :xcmenucmd Open in Next Editor<CR>
+      nmap <C-q> :xcmenucmd Close Editor<CR>:xcmenucmd Move Focus to Next Editor<CR>
+    '';
   };
 }
