f67397e091e2ecb14295964b1b103bbd94564525
Nathan Shelly <nsnshelly@gmail.com>
2020-10-17 11:12:43 -0700

    refactor gui & conditionally apply

--- nix/gui.nix
+++ nix/gui.nix
@@ -1,35 +1,7 @@
-{ vars }:
-
-let
-  GUI_APPS = "${vars.DOTFILES}/gui/apps";
-  PREFERENCES = " Library/Preferences ";
-  APPLICATION_SUPPORT = " Library/Preferences ";
-in
 {
+  # someday may manage other gui configs via nix but immutability prevents
+  # editing settings via the gui which is a dealbreaker for me at the moment
   home.file = {
-    # TODO: figure out better solution that allows for updating settings via gui
-    "${vars.XDG_CONFIG_HOME}/alacritty/alacritty.yml".source =
-      "${GUI_APPS}/alacritty/alacritty.yml";
-
-    # window organization/resizing
-    "${PREFERENCES_FOLDER}/com.knollsoft.Rectangle.plist".source =
-      "${GUI_APPS}/rectangle/com.knollsoft.Rectangle.plist";
-
-    # window switcher
-    "{PREFERENCES_FOLDER}/com.lwouis.alt-tab-macos.plist".source =
-      "${GUI_APPS}/alt-tab/com.lwouis.alt-tab-macos.plist";
-
-    # launcher (Alfred, Spotlight, Windows search, etc.)
-    "${APPLICATION_SUPPORT}/ueli/config.json".source =
-      "${GUI_APPS}/ueli/config.json";
-
-    # VSCode - text editor
-    "${APPLICATION_SUPPORT}/Code/User/keybindings.json".source =
-      "${GUI_APPS}/vscode/keybindings.json";
-
-    "${APPLICATION_SUPPORT}/Code/User/settings.json".source =
-      "${GUI_APPS}/vscode/settings.json";
-
     ".xvimrc".text = ''
       " Options
 
