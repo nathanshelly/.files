8e979497ccbab750380d6fac8e6a2bac0d20bc9c
Nathan Shelly <nsnshelly@gmail.com>
2020-10-17 22:51:29 -0700

    misc: add error message

--- infra/scripts/get_local_setting.sh
+++ infra/scripts/get_local_setting.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/env bash
 
-command -v jq > /dev/null || exit 1
+command -v jq > /dev/null || {
+  # shellcheck disable=2016
+  echo 'install `jq` to configure dotfiles via a `$HOME/.files-settings` json'
+  exit 1
+}
 
 # `-raw-output` strips quotes from strings
 # ref - https://github.com/stedolan/jq/issues/1735#issuecomment-427863218
