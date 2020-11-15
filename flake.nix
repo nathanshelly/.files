67ad8561cbe9050f83df55272aef52598378b448
Nathan Shelly <nsnshelly@gmail.com>
2020-11-14 22:08:47 -0800

    fix flake inputs

--- flake.nix
+++ flake.nix
@@ -2,19 +2,22 @@
   description = ".files";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-20.09-darwin";
+    # TODO: switch to unstable once macOS Big Sur support has landed
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-20.03-darwin";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/master";
+    # set home-manager's nipxkgs input to the nixpkgs specified in this flake
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-darwin.url = "github:lnl7/nix-darwin/master";
+    # set nix-darwin's nipxkgs input to the nixpkgs specified in this flake
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  # TODO: understand if `nixpkgs` argument here is 20.09 or following nix-darwin
   outputs = { self, nix-darwin, nixpkgs, home-manager }: {
     darwinConfigurations."smino" = nix-darwin.lib.darwinSystem {
-      modules = [ ./nix/darwin.nix ];
-      # modules = [ home-manager.darwinModules.home-manager ./nix/darwin.nix ];
+      # modules = [ ./nix/darwin.nix ];
+      modules = [ home-manager.darwinModules.home-manager ./nix/darwin.nix ];
     };
     # technically `nix-darwin` looks for a config specified by hostname
     # (not sure when, if ever, that's different from $HOST)
