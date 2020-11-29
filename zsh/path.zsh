26c198c20b1c28a60063d96f35cff0048bbe32fb
Nathan Shelly <nsnshelly@gmail.com>
2020-11-29 00:26:19 -0800

    misc cleanup

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -8,28 +8,11 @@
 # to prepend new entry - `path=(<new-addition> $path)`
 # to append new entry - `path+=<new-addition>` (`()` required to add >1 entry)
 modify_path() {
-  # poetry (python dependency manager) executables
-  [[ -d "$HOME/.poetry/bin" ]] && path=("$HOME/.poetry/bin" $path)
-
-  # `pip install --user` executables
-  [[ -d "$HOME/.local/bin" ]] && path=("$HOME/.local/bin" $path)
-
-  # cargo executables
-  [[ -d "$HOME/.cargo/bin" ]] && path=("$HOME/.cargo/bin" $path) # rust packages
-
-
   # put nix profile first on path
+  # TODO: update nix-darwin systempath setting to apply in zshrc vs zshenv then
+  # remove this
   [[ -d "/etc/profiles/per-user/$USER/bin" ]] && {
     path=("/etc/profiles/per-user/$USER/bin" $path)
   }
 }
 modify_path
-
-# TODO: remove w/ flakes
-modify_nix_path() {
-  # make frameworks managed via channels (nix-darwin, home-manager) available
-  export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
-  # make nix-darwin use the config in this repo
-  export NIX_PATH=darwin-config=$DOTFILES/nix/darwin.nix${NIX_PATH:+:}$NIX_PATH
-}
-modify_nix_path
