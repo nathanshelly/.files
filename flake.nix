1e471e1fb94dca9cda59edb526f18614bb9ddce9
Nathan Shelly <nsnshelly@gmail.com>
2022-12-25 14:36:12 -0800

    wip

--- flake.nix
+++ flake.nix
@@ -1,48 +1,25 @@
 {
-  description = ".files";
+  description = "my project description";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.fu.url = "github:numtide/flake-utils";
+  inputs.fu.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.11";
 
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
-
-    # neovim.url = "github:neovim/neovim?dir=contrib";
-    # neovim.inputs.nixpkgs.follows = "nixpkgs";
-
-    # TODO: investigate https://github.com/cachix/pre-commit-hooks.nix#nix-flakes-support
-    # pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-  };
-
-  # read thru this - https://github.com/malob/nixpkgs/blob/master/flake.nix
-
-  outputs = { self, nixpkgs, flake-utils }: {
-    devShells.aarch64-darwin.devShell.aarch64-darwin = nixpkgs.mkShell
-      {
-        packages = [ nixpkgs.gnumake ];
-
-        buildInputs = [ nixpkgs.ts-node ];
-
-        shellHook = ''
-          export DEBUG=1
-        '';
+  outputs = { self, nixpkgs, fu }: fu.lib.eachSystem fu.lib.defaultSystems (system:
+    let pkgs = nixpkgs.legacyPackages.${system};
+    in
+    rec {
+      devShells = {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ go gopls gotools go-tools ];
+        };
       };
-
-  };
-
+    });
   # flake-utils.lib.eachDefaultSystem
   #   (system:
   #     let pkgs = nixpkgs.legacyPackages.${system}; in
   #     {
-  #       devShell.default = pkgs.mkShell {
-  #         packages = [ pkgs.gnumake ];
-
-  #         inputsFrom = [ pkgs.hello pkgs.gnutar ];
-
-  #         shellHook = ''
-  #           export DEBUG=1
-  #         '';
-  #       };
+  #       devShells.default = import ./shell.nix { inherit pkgs; };
   #     }
   #   );
 }
